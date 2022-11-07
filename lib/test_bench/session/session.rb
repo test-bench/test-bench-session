cacf892446163a8c2b799a1490bc2ad032090bba
Nathan Ladd <nathanladd@gmail.com>
2022-11-07 08:57:10 -0600

    Failure (Session): Fail

--- lib/test_bench/session/session.rb
+++ lib/test_bench/session/session.rb
@@ -1,9 +1,26 @@
 module TestBench
   class Session
+    Failure = Class.new(RuntimeError)
+
+    def telemetry
+      @telemetry ||= TestBench::Telemetry::Substitute.build
+    end
+    attr_writer :telemetry
+
     def failure_sequence
       @failure_sequence ||= 0
     end
     attr_writer :failure_sequence
+
+    def fail(message=nil)
+      message ||= self.class.default_failure_message
+
+      record_failure
+
+      record_event(Events::Failed.new(message))
+
+      raise Failure, message
+    end
 
     def failed?(compare_sequence=nil)
       compare_sequence ||= 0
@@ -11,8 +28,16 @@ module TestBench
       compare_sequence != failure_sequence
     end
 
+    def record_event(event)
+      telemetry.record(event)
+    end
+
     def record_failure
       self.failure_sequence += 1
+    end
+
+    def self.default_failure_message
+      'Failed'
     end
   end
 end
