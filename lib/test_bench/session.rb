0698217b1715c82ef8c418019bd15f70ceb43c42
Nathan Ladd <nathanladd@gmail.com>
2022-11-07 08:57:10 -0600

    Failure (Session): Fail

--- lib/test_bench/session.rb
+++ lib/test_bench/session.rb
@@ -1,3 +1,5 @@
 require 'test_bench/output'
 
+require 'test_bench/session/events'
+
 require 'test_bench/session/session'
---
+++ lib/test_bench/session/controls/random.rb
@@ -0,0 +1,7 @@
+module TestBench
+  class Session
+    module Controls
+      Random = TestBench::Output::Controls::Random
+    end
+  end
+end
