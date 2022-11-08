70c420cacc7e6a5dfb63d15d6d218786fd82bcfb
Nathan Ladd <nathanladd@gmail.com>
2022-11-07 20:44:15 -0600

    Context (Session): Variant

--- lib/test_bench/session/session.rb
+++ lib/test_bench/session/session.rb
@@ -1,6 +1,7 @@
 module TestBench
   class Session
     Failure = Class.new(RuntimeError)
+    Abort = Class.new(Failure)
 
     def telemetry
       @telemetry ||= TestBench::Telemetry::Substitute.build
@@ -21,6 +22,13 @@ module TestBench
       @skip_sequence ||= 0
     end
     attr_writer :skip_sequence
+
+    def context!(...)
+      if context(...) == false
+        message = Session.abort_message
+        raise Abort, message
+      end
+    end
 
     def context(title=nil, &block)
       if block.nil?
@@ -105,6 +113,10 @@ module TestBench
 
     def self.assertion_failure_message
       "Assertion failed"
+    end
+
+    def self.abort_message
+      "Abort"
     end
   end
 end
