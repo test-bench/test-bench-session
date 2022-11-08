306cb59da5c46fca94a91fc04fdf16cb8ddb0c1c
Nathan Ladd <nathanladd@gmail.com>
2022-11-07 20:29:33 -0600

    Context (Session): Context

---
+++ test/automated/session/context/context_started.rb
@@ -0,0 +1,36 @@
+require_relative '../../automated_init'
+
+context "Session" do
+  context "Context" do
+    context "Context Started Event" do
+      session = Session.new
+
+      control_title = Controls::Title::Context.example
+
+      session.context(control_title) do
+        #
+      end
+
+      context "Context Started Event" do
+        context_started = session.telemetry.one_event(Session::Events::ContextStarted)
+
+        test! "Recorded" do
+          refute(context_started.nil?)
+        end
+
+        context "Attributes" do
+          context "Title" do
+            title = context_started.title
+
+            comment title.inspect
+            detail "Control: #{control_title.inspect}"
+
+            test do
+              assert(title == control_title)
+            end
+          end
+        end
+      end
+    end
+  end
+end
