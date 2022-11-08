e7b770345afc7589bfc6b3dcf90148a853f99730
Nathan Ladd <nathanladd@gmail.com>
2022-11-07 20:10:27 -0600

    Test (Session): Variant

---
+++ test/automated/session/test/variant/test_finished.rb
@@ -0,0 +1,49 @@
+require_relative '../../../automated_init'
+
+context "Session" do
+  context "Test Variant" do
+    context "Test Finished Event" do
+      session = Session.new
+
+      control_title = Controls::Title::Test.example
+
+      begin
+        session.test!(control_title) do
+          #
+        end
+      rescue Session::Abort
+      end
+
+      context "Test Finished Event" do
+        test_finished = session.telemetry.one_event(Session::Events::TestFinished)
+
+        test! "Recorded" do
+          refute(test_finished.nil?)
+        end
+
+        context "Attributes" do
+          context "Title" do
+            title = test_finished.title
+
+            comment title.inspect
+            detail "Control: #{control_title.inspect}"
+
+            test do
+              assert(title == control_title)
+            end
+          end
+
+          context "Result" do
+            result = test_finished.result
+
+            comment result.inspect
+
+            test "Set" do
+              refute(result.nil?)
+            end
+          end
+        end
+      end
+    end
+  end
+end
