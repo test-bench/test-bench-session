d43a9d56f8c69c797a2a5e3fdafaa49e5074f615
Nathan Ladd <nathanladd@gmail.com>
2022-11-10 16:59:32 -0600

    Predicates (Session Substitute): Aliases

--- lib/test_bench/session/substitute.rb
+++ lib/test_bench/session/substitute.rb
@@ -47,6 +47,24 @@ module TestBench
           RUBY
         end
 
+        alias :failure? :failed_event?
+        alias :one_failure? :one_failed_event?
+
+        alias :test? :test_finished_event?
+        alias :one_test? :one_test_finished_event?
+
+        alias :context? :context_finished_event?
+        alias :one_context? :one_context_finished_event?
+
+        alias :comment? :commented_event?
+        alias :one_comment? :one_commented_event?
+
+        alias :detail? :detailed_event?
+        alias :one_detail? :one_detailed_event?
+
+        alias :fixture? :fixture_finished_event?
+        alias :one_fixture? :one_fixture_finished_event?
+
         def one_event?(...) = sink.one_event?(...)
         def one_event(...) = sink.one_event(...)
         def any_event?(...) = sink.any_event?(...)
