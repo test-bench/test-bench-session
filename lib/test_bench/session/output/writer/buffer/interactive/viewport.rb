906e04e2ca04ef6835baf970fca5f9ef7cdd35ae
Nathan Ladd <nathanladd@gmail.com>
2023-05-17 15:25:08 -0500

    Viewport (Interactive Writer's Buffer): Capacity

---
+++ lib/test_bench/session/output/writer/buffer/interactive/viewport.rb
@@ -0,0 +1,44 @@
+module TestBench
+  class Session
+    class Output
+      class Writer
+        module Buffer
+          class Interactive
+            Viewport = Struct.new(:width, :height, :row, :column, :scroll_rows, :rows_scrolled) do
+              def self.build(width, height, row, column, scroll_rows=nil)
+                scroll_rows ||= 0
+
+                rows_scrolled = 0
+
+                new(width, height, row, column, scroll_rows, rows_scrolled)
+              end
+
+              def self.null
+                build(0, 0, 0, 0)
+              end
+
+              def capacity
+                capacity = 0
+
+                rows_remaining = height + scroll_rows_remaining - row - 1
+
+                if rows_remaining > 0
+                  capacity += (rows_remaining - 1) * width
+
+                  final_row = width - column
+                  capacity += final_row
+                end
+
+                capacity
+              end
+
+              def scroll_rows_remaining
+                scroll_rows - rows_scrolled
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
