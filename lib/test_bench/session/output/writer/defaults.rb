679bba30fcb466a777514d023d0093a784505fb8
Nathan Ladd <nathanladd@gmail.com>
2023-05-18 16:06:13 -0500

    Buffer (Output Writer): Configure Buffer

--- lib/test_bench/session/output/writer/defaults.rb
+++ lib/test_bench/session/output/writer/defaults.rb
@@ -6,6 +6,10 @@ module TestBench
           def self.device
             TestBench::Output::Writer::Defaults.device
           end
+
+          def self.experimental_output
+            ENV.fetch('TEST_BENCH_EXPERIMENTAL_OUTPUT', 'off') == 'on'
+          end
         end
       end
     end
